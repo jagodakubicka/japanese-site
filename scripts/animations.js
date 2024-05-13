a799a912b4dc0e1c2d849925be20d6ae21140d0e
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2024-05-13 18:51:28 +0200

    animate bg circles in hero section

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,4 +1,33 @@
 const footer = document.querySelector('.footer');
+const cards = document.querySelector('#what-we-do');
+
+const heroBgCircleAnimation = anime({
+  targets: '.hero-main__overlap-circle--second',
+  translateX: function () {
+    return anime.random(-200, 200);
+  },
+  translateY: function () {
+    return anime.random(-200, 200);
+  },
+  direction: 'alternate',
+  easing: 'linear',
+  duration: 8000,
+  loop: true,
+});
+
+const heroBgTopCircleAnimation = anime({
+  targets: '.hero-main__overlap-circle--first',
+  translateX: function () {
+    return anime.random(-200, 200);
+  },
+  translateY: function () {
+    return anime.random(-100, 100);
+  },
+  direction: 'alternate',
+  easing: 'linear',
+  duration: 8000,
+  loop: true,
+});
 
 const footerCircleAnimation = anime({
   targets: '.footer-circle',
@@ -13,9 +42,19 @@ const animateElement = function (div, speed = 400, offset = 800) {
   const scrollPercent = window.scrollY - div.offsetTop;
   return (scrollPercent + offset) / speed;
 };
+const origamiCardAnimation = anime({
+  targets: '.what-we-do__card--animated',
+  translateY: -200,
+  autoplay: false,
+  delay: anime.stagger(400),
+  easing: 'easeInOutQuad',
+});
 
 window.onscroll = function () {
   footerCircleAnimation.seek(
     animateElement(footer) * footerCircleAnimation.duration
+  );
+  origamiCardAnimation.seek(
+    animateElement(cards, 1000) * origamiCardAnimation.duration
   );
 };
